8dedbe16b0eef1d2507b7471d6323bfe796c8989
kalture <ziggynyathi091@gmail.com>
2023-05-26 11:53:38 +0200

    first commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Naruto T-shirt",
+    price: 250,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Naruto b&w T-shirt",
+    price: 250,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Dragonball T-shirt",
+    price: 250,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 450,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Naruto Strips",
+    price: 250,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 350,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 300,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 200,
+    productImage: product8,
+  },
+];
